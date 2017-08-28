8e707c44055a6b2984dc04ce2be6e39e84b73819
Hui Jiang <huijiang@Huis-MBP.home>
2017-08-27 22:04:28 -0400

    added merge sort from chapter 2

--- Chapter2.js
+++ Chapter2.js
@@ -10,6 +10,8 @@ function insertionSort(array) {
 	for (var j = 1; j < array.length; j++) {
 		let key = array[j]
 		let i = j - 1;
+
+		//change the > key to < key to sort array in decreasing order
 		while (i >= 0 && array[i] > key) {
 			array[i + 1] = array[i]
 			i --;
@@ -21,3 +23,44 @@ function insertionSort(array) {
 console.log(array);
 insertionSort(array);
 console.log(array);
+
+//Chapter 2: Merge sort page 31 & 34
+//Merge Sort run time O(n lg n)
+//sort subarray in array from index p to r
+function mergeSort(array, p, r){
+	if (p < r) {
+		var q = Math.floor((p+r)/2);
+		mergeSort(array, p, q);
+		mergeSort(array, q+1, r);
+		merge(array, p, q, r);
+	}
+}
+
+//p, q r are indices in the array such that p <= q < r
+//array[p..q] & array[q+1..r] are sorted subarray
+function merge(array, p, q, r){
+	var left = array.slice(p, q+1);
+	var right = array.slice(q+1, r+1);
+
+	var sentinel = array[q] > array[r] ? array[q] + 1 : array[r] + 1;	
+	
+	left.push(sentinel);
+	right.push(sentinel);
+	
+	var i = 0;
+	var j = 0;
+	for (var k = p; k <= r; k++){
+		if (left[i] <= right[j]){
+			array[k] = left[i];
+			i++;
+		} else {
+			array[k] = right[j];
+			j++;
+		}
+	}
+	
+}
+
+var arrayMerge = [27, 26, 5, 45, 9, 11, 13, 3, 4, 16, 28, 10, 34];
+mergeSort(arrayMerge, 0, arrayMerge.length-1);
+console.log(arrayMerge);
